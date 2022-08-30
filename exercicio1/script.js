9d46bdc58924fd2da22687c3125d368059beb52d
gustavojb1 <gustavojunqueirab@gmail.com>
2022-08-30 14:51:32 -0300

    correção

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,13 +1,14 @@
 let numero = prompt("digite um número par")
-let resultado = numero%2
 
-resultado = Number(resultado)
+numero = Number(numero)
+
+let resultado = numero%2
 
 console.log(resultado)
 
 // sempre que inserido um número par o resultado é 0
 
-// quando é colocado número ímpar o resto é diferente de 0
+// quando é colocado número ímpar o resto é 1
 
 // quando o javascript faz uma conta com string e o resultado é um número ele transforma automáticamente para número
 
